99ece4aa62bbd66a2d0abe2d834d44ec8fac1784
tichdaen <tichdaen@gmail.com>
2023-11-12 14:04:50 +0900

    remove comment

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-// Handler
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query
   return res.json({
