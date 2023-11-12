5536bad4fa4788e516d4dc214de9d81f4cd6cbdc
tichdaen <tichdaen@gmail.com>
2023-11-12 14:58:08 +0900

    change date format

--- api/test.ts
+++ api/test.ts
@@ -2,11 +2,15 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse): VercelResponse {
 
+    const date = new Date()
+    const now = `${date.getFullYear()}-${date.getMonth() - 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+
+
     return res.json({
         queries: req.query,
         cookies: req.cookies,
         body: req.body,
         test: 'Hello, W0rld!',
-        lastRequestTime: new Date()
+        lastRequestTime: now
     })
 }
